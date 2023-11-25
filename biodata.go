dbe600d07203a2a09569fd4740d16fc720284031
ocean <145195245+nesyass@users.noreply.github.com>
2023-11-25 23:28:45 +0700

    first commit

---
+++ biodata.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nama := "Nesya Sulistyawati"
+	nim := "103032300039"
+	kelas := "IT-47-01"
+	hobi := "Membaca"
+	umur := "18 Tahun"
+	mbti := "INTP"
+
+	fmt.Println("Biodata")
+	fmt.Println("Nama   :", nama)
+	fmt.Println("NIM    :", nim)
+	fmt.Println("Kelas  :", kelas)
+	fmt.Println("Hobi   :", hobi)
+	fmt.Println("Umur   :", umur)
+	fmt.Println("MBTI   :", mbti)
+}
